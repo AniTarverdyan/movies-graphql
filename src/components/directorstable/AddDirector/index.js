0558b956073f091fba0d27e18a868ffa69e5ac21
Ani <ani.tarverdyan.87@gmail.com>
2022-07-04 14:12:45 +0400

    ...

--- src/components/directorstable/AddDirector/index.js
+++ src/components/directorstable/AddDirector/index.js
@@ -1,38 +1,42 @@
+import { useQuery } from "@apollo/client";
 import { Input, InputLabel } from "@material-ui/core";
 import React, { useState } from "react";
-import { useMutation } from "react-apollo";
-import { addDirectorMutation } from "../mutation";
 import { directorQuery } from "../queries";
 import "./style.css";
 
 const AddDirector = () => {
-  const [directors, setDirectors] = useState([
-    {
-      name: "",
-      age: "",
-    },
-  ]);
-  const [inputName, setInputName] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  // const { loading, error, data } = useQuery(directorQuery);
+  // console.log(data);
 
-  const handleChangeName = (e) => {
-    setInputName(e.target.value);
-  };
+  // const [directors, setDirectors] = useState([
+  //   {
+  //     name: "",
+  //     age: "",
+  //   },
+  // ]);
+  // const [inputName, setInputName] = useState("");
+  // const [inputAge, setInputAge] = useState("");
 
-  const handleChangeAge = (e) => {
-    setInputAge(e.target.value);
-  };
+  // const handleChangeName = (e) => {
+  //   setInputName(e.target.value);
+  // };
 
-const addDirectorData = (e) => {
-  setDirectors([{
-    name: inputName,
-    age: inputAge
-  }])
-}
+  // const handleChangeAge = (e) => {
+  //   setInputAge(e.target.value);
+  // };
+
+  // const addDirectorData = (e) => {
+  //   setDirectors([
+  //     {
+  //       name: inputName,
+  //       age: inputAge,
+  //     },
+  //   ]);
+  // };
 
   return (
     <div className="director">
-      <div>
+      {/* <div>
         <InputLabel htmlFor="component-helper">Director's Name</InputLabel>
         <Input onChange={handleChangeName} value={inputName} />
       </div>
@@ -43,18 +47,18 @@ const addDirectorData = (e) => {
       <div>
         <button onClick={addDirectorData}>Add</button>
       </div>
-      {directors.map((director) => {
+      {/* {data.directors.map((item) => {
         return (
           <div className="director-data-box">
-            <div>Name:{director.name}</div>
-            <div>Age:{director.age}</div>
+            <div>Name:{item.name}</div>
+            <div>Age:{item.age}</div>
             <div className="button">
               <button>Edit</button>
               <button>Delete</button>
             </div>
           </div>
         );
-      })}
+      })} */} 
     </div>
   );
 };
--- src/components/directorstable/DirectorsTable.js
+++ src/components/directorstable/DirectorsTable.js
@@ -1,52 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
+import "./style.css";
 
 import { directorQuery } from "./queries";
 import { deleteDirectorMutation } from "./mutation";
+import { Input, InputLabel } from "@material-ui/core";
 
 const DirectorsTable = () => {
   const { loading, error, data } = useQuery(directorQuery);
+  const [director, setDirectors] = useState([]);
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+  
+  useEffect(() => {
+    if(data) {
+      setDirectors([
+        ...data,
+        {
+          name: data.directors.name,
+          age: data.directors.age,
+        },
+      ]);
+      console.log(director)
+    }
 
+  },[])
+  console.log(data);
   if (loading) return <p>Loading...</p>;
 
+  const handleChangeName = (e) => {
+    setInputName(e.target.value);
+  };
+
+  const handleChangeAge = (e) => {
+    setInputAge(e.target.value);
+  };
 
 
-// let a;
-
-//   const addDirector = () => {
-
-//   }
-//   console.log(data.directors);
-
-//   const deleteDirector = () => () => {
-//    const deleteMutation = useQuery(deleteDirectorMutation, {variables: {id: 1}})
-//   }
-  
+  const addDirectorData = () => {
+  };
 
   return (
-    <div>
-      {/* <input onChange={handleChange} value={inputValue} />
-      <button onClick={addDirector}>Add Director</button>
-      <button>Return</button> */}
-      {/* {directors} */}
-      {/* <table>
-        <thead>
-          <tr>
-            <td>Name</td>
-            <td>Age</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data.directors.map((item) => {
-            return (
-              <tr >
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table> */}
+    <div className="director">
+      <div>
+        <InputLabel htmlFor="component-helper">Director's Name</InputLabel>
+        <Input onChange={handleChangeName} value={inputName} />
+      </div>
+      <div>
+        <InputLabel htmlFor="component-helper">Director's Age</InputLabel>
+        <Input type="number" onChange={handleChangeAge} value={inputAge} />
+      </div>
+      <div>
+        <button onClick={addDirectorData}>Add</button>
+      </div>
+      {/* {data.directors.map((item) => {
+        return (
+          <div className="director-data-box">
+            <div>Name: {item.name}</div>
+            <div>Age: {item.age}</div>
+          </div>
+        );
+      })} */}
+      {director}
     </div>
   );
 };
